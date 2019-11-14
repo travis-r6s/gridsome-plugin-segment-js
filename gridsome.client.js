b5741fe8736212da1fc1ebf82950189eb3563593
Travis Reynolds <travis@cirruscreative.co.uk>
2019-11-14 21:49:17 +0000

    add demo with segment-node

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,7 +1,10 @@
-export default (Vue, options, { head, router }) => {
-  const { trackPage = true, prodKey, devKey, debug } = options
+import Analytics from 'analytics-node'
+import nanoid from 'nanoid'
 
-  if (process.isClient) {
+export default (Vue, options, { head, isServer, router }) => {
+  const { trackPage, prodKey, devKey } = options
+
+  if (!isServer) {
     // ensures Segment write key is present
     if (!prodKey || prodKey.length < 10) { console.error('segment prodKey must be at least 10 char in length') }
 
@@ -11,39 +14,20 @@ export default (Vue, options, { head, router }) => {
     // use prod write key when in prod env, else use dev write key
     // note below, snippet wont render unless writeKey is truthy
     const writeKey = process.env.NODE_ENV === 'production' ? prodKey : devKey
-    head.script.push({
-      src: `https://cdn.segment.com/analytics.js/v1/${writeKey}/analytics.min.js`,
-      body: true
-    })
 
-    const analytics = window.analytics = window.analytics || []
-    if (!analytics.initialize) {
-      if (analytics.invoked) window.console && console.error && console.error('Segment snippet included twice.')
-      else {
-        analytics.invoked = true
-        analytics.methods = ['trackSubmit', 'trackClick', 'trackLink', 'trackForm', 'pageview', 'identify', 'reset', 'group', 'track', 'ready', 'alias', 'debug', 'page', 'once', 'off', 'on']
-        analytics.factory = function (t) {
-          return function () {
-            var e = Array.prototype.slice.call(arguments)
-            e.unshift(t)
-            analytics.push(e)
-            return analytics
-          }
-        }
-        for (var t = 0; t < analytics.methods.length; t++) {
-          var e = analytics.methods[ t ]
-          analytics[ e ] = analytics.factory(e)
-        }
+    const analytics = new Analytics(writeKey)
 
-        analytics.SNIPPET_VERSION = '4.1.0'
-      }
-    }
+    Vue.prototype.$analytics = analytics
 
     if (trackPage) {
       router.afterEach((to, from) => {
-        window.analytics.page(to.path || '', {
-          path: to.fullPath,
-          referrer: from.fullPath
+        analytics.page({
+          anonymousId: nanoid(10),
+          name: to.path || '',
+          properties: {
+            path: to.fullPath,
+            referrer: from.fullPath
+          }
         })
       })
     }
@@ -53,6 +37,5 @@ export default (Vue, options, { head, router }) => {
 export const defaultOptions = () => ({
   prodKey: '',
   devKey: '',
-  trackPage: true,
-  debug: false
+  trackPage: true
 })
